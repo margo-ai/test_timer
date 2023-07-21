cc5a8dc423b3f84c3ba70a09033a7141563b317a
Margo <plokhova.margarita@yandex.ru>
2023-07-21 16:13:25 +0300

    timer created

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
-
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
+let hours;
+let minutes;
+let seconds;
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
-const createTimerAnimator = () => {
-  return (seconds) => {};
+const getZero = (num) => {
+	return num >= 0 && num < 10 ? `0${num}` : num;
 };
 
-const animateTimer = createTimerAnimator();
+const createTimerAnimator = (time) => {
+	let timer = setInterval(() => {
+		hours = getZero(Math.floor(time / 60 / 60));
+		minutes = getZero(Math.floor(time / 60) - hours * 60);
+		seconds = getZero(Math.floor(time % 60));
+		if (time <= 0) {
+			clearInterval(timer);
+			timerEl.innerHTML = "Время закончилось";
+		} else {
+			let timeString = `${hours}:${minutes}:${seconds}`;
+			timerEl.innerHTML = timeString;
+		}
+		--time;
+	}, 1000);
+};
 
-inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
+// const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener("input", () => {
+	// Очистите input так, чтобы в значении
+	// оставались только числа
+	inputEl.value = inputEl.value.replace(/[^0-9]/g, "");
+	console.log(inputEl.value);
 });
 
-buttonEl.addEventListener('click', () => {
-  const seconds = Number(inputEl.value);
-
-  animateTimer(seconds);
-
-  inputEl.value = '';
+buttonEl.addEventListener("click", () => {
+	const time = Number(inputEl.value);
+	createTimerAnimator(time);
+	inputEl.value = "";
 });
